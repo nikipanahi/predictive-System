6789721d0ca6b176185a41d2c25e3c189454b709
Niki Panahi <nikipanahi21@gmail.com>
2026-07-24 12:51:15 +0330

    Update urls.py

--- Aviation project files/inventory_site/urls.py
+++ Aviation project files/inventory_site/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('Tamo.urls')),
+    path('', include('mro_app.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
