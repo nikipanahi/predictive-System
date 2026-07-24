4515a13f128463ffa0681892f1828a101ff28fab
Niki Panahi <nikipanahi21@gmail.com>
2026-07-24 15:14:38 +0330

    Rename Tamo folder to mro_app

--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views # یعنی از همین پوشه فایل views را بیاور
-
-urlpatterns = [
-    path('api/sync/', views.export_for_offline, name='export_offline'),
-]
